f303d1542ef881e3cbaaf1bc8a724731afdd7190
Kunsági Zsolt <kunsagi.zsolt2@telekom.hu>
2020-12-22 13:26:18 +0100

    1

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,6 +1,8 @@
 import firebase from "firebase";
 const firebaseConfig = {
 
+  //Ha csak te használod a projektet, akkor a lenti helyett ide másolhatod a Firestore kapcsolódás adataid.
+  // De a felmásolod a Githubra, akkor a .env módszert kövesd.
 
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
